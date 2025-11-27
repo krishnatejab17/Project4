4dfd8e92e3f7f689e79c9f24087cc772b920518b
Krishnatejab17 <17bdevops@gmail.com>
2025-11-27 12:04:41 +0000

    test commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "project1" {
+  name = "project1-repo"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+    
+  }
+}
+
