65da0e9f293a432120af4c08a8d066b1f748c048
Krishnatejab17 <17bdevops@gmail.com>
2025-11-29 16:09:40 +0000

    test_3_project1

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,6 +1,10 @@
+###############################################
+# ECS Task Execution Role
+###############################################
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "${var.app_name}-execution-task-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+
   tags = {
     Name        = "${var.app_name}-iam-role"
     Environment = var.app_environment
@@ -23,18 +27,38 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+
+###############################################
+# GitHub OIDC Provider
+###############################################
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+
+###############################################
+# GitHub Actions OIDC Role (Corrected)
+###############################################
 resource "aws_iam_role" "github_actions_oidc_role" {
   name = "github-actions-oidc-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
+        Action = "sts:AssumeRoleWithWebIdentity",
         Principal = {
-          Federated = "arn:aws:iam::828411126532:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
@@ -44,4 +68,39 @@ resource "aws_iam_role" "github_actions_oidc_role" {
       }
     ]
   })
+}
+
+
+###############################################
+# GitHub OIDC Role Permissions
+###############################################
+
+# ECR push/pull
+resource "aws_iam_role_policy_attachment" "github_actions_ecr_policy" {
+  role       = aws_iam_role.github_actions_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+# ECS deploy
+resource "aws_iam_role_policy_attachment" "github_actions_ecs_policy" {
+  role       = aws_iam_role.github_actions_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECSFullAccess"
+}
+
+# Terraform needs to create VPC/Subnets/Security Groups/ALB
+resource "aws_iam_role_policy_attachment" "github_actions_vpc_policy" {
+  role       = aws_iam_role.github_actions_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+# Terraform needs to create IAM roles (like ECS Task Role, Execution Role)
+resource "aws_iam_role_policy_attachment" "github_actions_iam_policy" {
+  role       = aws_iam_role.github_actions_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+# CloudWatch logs for ECS tasks
+resource "aws_iam_role_policy_attachment" "github_actions_cloudwatch_policy" {
+  role       = aws_iam_role.github_actions_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
