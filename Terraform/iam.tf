a776ef91d97eca1858693570358e092b35e1cc4c
Krishnatejab17 <17bdevops@gmail.com>
2025-11-29 15:45:14 +0000

    added GITHUB OIDC role

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -22,3 +22,31 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role" "github_actions_oidc_role" {
+  name = "github-actions-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::828411126532:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:Krishna/PROJECT:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_oidc_policy" {
+  role       = aws_iam_role.github_actions_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # or restrict to ECR/ECS permissions
+}
